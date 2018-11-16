b6b5c605e87fc5586b434deea9ab6baf784edcef
webfrontzhifei <906831852@qq.com>
2018-11-16 15:22:49 +0800

    fix:demo

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Video from './components/videoplayer';
-
+import { Provider } from 'mobx-react';
+import TodoListModel from './models/TodoListModel';
+import TodoList from './components/TodoList';
 class App extends Component {
+  constructor(opts) {
+    super(opts);
+    this.store = new TodoListModel();
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-            <Video src="https://video1.pinduoduo.com/i2/20181102/34ce52dfa50842138d54d5190282b679.mp4.f20.mp4"></Video>
-        </header>
-      </div>
+      <Provider store={this.store}>
+        <div className="App">
+          <TodoList></TodoList>
+        </div>
+      </Provider>
     );
   }
 }
---
+++ src/components/TodoList/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { observable, action } from 'mobx';
+import { observer } from 'mobx-react';
+
+class TodoList extends React.Component {
+    render() {
+        return <div>list</div>
+    }
+}
+
+export default TodoList;
